f49cd8a77633a1fb8f8d24192284dc61cb2578f9
장준하 <jangjunha@jangjunhaui-MacBookPro.local>
2024-07-22 18:34:17 +0900

    initial commit

---
+++ src/test/java/newCar/event_page/EventPageApplicationTests.java
@@ -0,0 +1,13 @@
+package newCar.event_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
