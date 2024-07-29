41e91278f1d11dc55ef70b35e7b467e692c90a18
bjh3311 <bjh3311@naver.com>
2024-07-29 17:31:49 +0900

    TASK-54 EventCommonRepository 생성

---
+++ src/main/java/newCar/event_page/repository/EventCommonRepository.java
@@ -0,0 +1,7 @@
+package newCar.event_page.repository;
+
+import newCar.event_page.entity.event.EventCommon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventCommonRepository extends JpaRepository<EventCommon,Long> {
+}
--- src/main/java/newCar/event_page/service/EventService.java
+++ src/main/java/newCar/event_page/service/EventService.java
@@ -14,6 +14,7 @@ import java.util.NoSuchElementException;
 @Service
 public class EventService {
 
+
     private final EventRepository eventRepository;
 
     @Autowired
