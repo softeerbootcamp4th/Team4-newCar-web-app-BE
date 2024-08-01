4f4be016eed049aa1e1575e2a57de2126376ef8e
bjh3311 <bjh3311@naver.com>
2024-08-01 13:55:00 +0900

    TASK-54 drawWinner메소드 리팩토링

--- src/main/java/newCar/event_page/service/RacingService.java
+++ src/main/java/newCar/event_page/service/RacingService.java
@@ -27,6 +27,8 @@ public class RacingService {
     private final RacingWinnerRepository racingWinnerRepository;
     private final RacingEventRepository racingEventRepository;
 
+    private double totalWeight;
+
     public List<PersonalityTestDTO> getPersonalityList() {
         List<PersonalityTest> list = personalityTestRepository.findAll();
         List<PersonalityTestDTO> personalityTestDTOList = new ArrayList<>();
@@ -48,26 +50,17 @@ public class RacingService {
         racingWinnerRepository.deleteByEventId(eventId);//deleteById
         List<EventUser> eventUserList = eventUserRepository.findByEventId(eventId); //Racing게임을 참가한 사람들의 목록을 받아온다
         Set<Participant> participantSet = new LinkedHashSet<>();
-        double totalWeight = 0;
+        totalWeight = 0;
         double weight = 0;
-
         for (EventUser eventUser : eventUserList) {
             weight = getWeight(eventUser.getUser().getClickNumber());
             participantSet.add(new Participant(eventUser.getUser().getId(), weight));
             totalWeight += weight;
         }//각 참가자의 가중치와 전체 가중치를 구해 준다
-        for (WinnerSettingDTO winnerSettingDTO : winnerSettingDTOList) {
-            int numberOfWinners = winnerSettingDTO.getNum();// 각 등수별로 몇명 뽑는지 넘어온 설정 값으로 참조
-            int rank = winnerSettingDTO.getRank(); //이번 추첨은 어떤 랭크인지
-            for (int i = 0; i < numberOfWinners; i++) {
-                Participant winner = getWinner(participantSet,totalWeight);
-                totalWeight = totalWeight  - winner.weight; //전체가중치에서 당첨자의 가중치를 빼준다
-                participantSet.remove(winner);//중복 제거를 위해 Set에서 제거 해준다
-                racingWinnerRepository.save(new RacingWinner(racingEventRepository.getReferenceById(eventId),
-                        eventUserRepository.findByUserIdAndEvendId(winner.userId, eventId),rank));
-            }
-        }
+        setWinners(winnerSettingDTOList, eventId, participantSet);
     }
+
+
 
     public int getEventUserSize(Long eventId) {
         return eventUserRepository.findByEventId(eventId).size();
@@ -82,8 +75,21 @@ public class RacingService {
         return racingWinnersDTOList;
     }
 
+    private void setWinners(List<WinnerSettingDTO> winnerSettingDTOList, Long eventId, Set<Participant> participantSet) {
+        for (WinnerSettingDTO winnerSettingDTO : winnerSettingDTOList) {
+            int numberOfWinners = winnerSettingDTO.getNum();// 각 등수별로 몇명 뽑는지 넘어온 설정 값으로 참조
+            int rank = winnerSettingDTO.getRank(); //이번 추첨은 어떤 랭크인지
+            for (int i = 0; i < numberOfWinners; i++) {
+                Participant winner = drawOneWinner(participantSet,totalWeight);
+                totalWeight -= winner.weight; //전체가중치에서 당첨자의 가중치를 빼준다
+                participantSet.remove(winner);//중복 제거를 위해 Set에서 제거 해준다
+                racingWinnerRepository.save(new RacingWinner(racingEventRepository.getReferenceById(eventId),
+                        eventUserRepository.findByUserIdAndEvendId(winner.userId, eventId),rank));
+            }
+        }
+    }
 
-    private Participant getWinner(Set<Participant> participantSet,double totalWeight){
+    private Participant drawOneWinner(Set<Participant> participantSet,double totalWeight){
         Random rand = new Random();
         int maxInt = (int)totalWeight; //totalWeight가 8.45일 경우 8이 저장됨
         int randomInt = rand.nextInt(maxInt) + 1; // 1~8이 저장됨
