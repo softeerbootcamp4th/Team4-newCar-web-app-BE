658697bfbcbc95afb5ded5766e7743b4c24edff6
bjh3311 <bjh3311@naver.com>
2024-08-02 20:04:44 +0900

    Admin 리팩토링 끝

--- src/main/java/newCar/event_page/service/QuizService.java
+++ src/main/java/newCar/event_page/service/QuizService.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
