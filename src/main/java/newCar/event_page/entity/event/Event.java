75827f98e36e2db416de646ed8b09887a365e27b
장준하 <jangjunha@jangjunhaui-MacBookPro.local>
2024-08-03 06:20:42 +0900

    이벤트 진행 기간을 알아내는 로직은 Event 객체의 책임이다.

--- src/main/java/newCar/event_page/entity/event/Event.java
+++ src/main/java/newCar/event_page/entity/event/Event.java
@@ -3,6 +3,9 @@ package newCar.event_page.entity.event;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 @Entity
 @Getter
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -16,4 +19,10 @@ public class Event {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="EVENT_COMMON_ID")
     private EventCommon eventCommon;
+
+    public long getDuration(){
+        LocalDate startDate = eventCommon.getStartTime().toLocalDate();
+        LocalDate endDate = eventCommon.getEndTime().toLocalDate();
+        return ChronoUnit.DAYS.between(startDate, endDate) + 1;
+    }
 }
