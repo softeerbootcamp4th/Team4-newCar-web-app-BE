9cf823457f7639f8810f0c3839c1b1eedb0ea343
장준하 <jangjunha@jangjunhaui-MacBookPro.local>
2024-08-05 05:00:19 +0900

    JPA 쿼리메서드 사용으로 변경

--- src/main/java/newCar/event_page/repository/EventUserRepository.java
+++ src/main/java/newCar/event_page/repository/EventUserRepository.java
@@ -8,9 +8,14 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface EventUserRepository extends JpaRepository<EventUser,Long> {
-    @Query(value = "select * from event_user where event_id = :eventId", nativeQuery = true)
-    List<EventUser> findByEventId(@Param("eventId") Long eventId);
+    /*@Query(value = "select * from event_user where event_id = :eventId", nativeQuery = true)
+    List<EventUser> findByEventId(@Param("eventId") Long eventId);*/
 
-    @Query(value = "select * from event_user where user_id = :userId AND event_id = :eventId" , nativeQuery = true)
-    EventUser findByUserIdAndEvendId(@Param("userId") Long userId , @Param("eventId") Long eventId);
+    EventUser findByUserIdAndEventId(Long userId, Long eventId);
+
+    /*@Query(value = "select * from event_user where user_id = :userId AND event_id = :eventId" , nativeQuery = true)
+    EventUser findByUserIdAndEvendId(@Param("userId") Long userId , @Param("eventId") Long eventId);*/
+
+    List<EventUser> findByEventId(Long EventId);
+
 }
