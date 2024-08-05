0bf2cc752bc15c5052853d5c7c782d5055047b6e
장준하 <jangjunha@jangjunhaui-MacBookPro.local>
2024-08-05 18:36:31 +0900

    quiz레포 메서드 추가

--- src/main/java/newCar/event_page/repository/quiz/QuizRepository.java
+++ src/main/java/newCar/event_page/repository/quiz/QuizRepository.java
@@ -10,5 +10,8 @@ import java.util.List;
 
 
 public interface QuizRepository extends JpaRepository<Quiz,Long> {
+    //JPA에서는 메서드 이름 명명규칙을 통해 쿼리를 사용할 수 있는 기능을 제공한다.
+    List<Quiz> findAllByOrderByPostDateAsc();
+
     List<Quiz> findAllByOrderByIdAsc();
 }
