b967cb182654ba72135a60fc98c13f08b0d4f8e1
bjh3311 <bjh3311@naver.com>
2024-08-06 16:11:09 +0900

    TASK-151  SessionService 작성 및 세션 유효성 검사 메소드 작성

---
+++ src/main/java/newCar/event_page/service/SessionService.java
@@ -0,0 +1,16 @@
+package newCar.event_page.service;
+
+import lombok.RequiredArgsConstructor;
+import newCar.event_page.model.SessionStorage;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SessionService {
+
+    private final SessionStorage sessionStorage;
+
+    public boolean validateSession(String sessionId) {
+        return sessionStorage.isValidSession(sessionId);
+    }
+}
