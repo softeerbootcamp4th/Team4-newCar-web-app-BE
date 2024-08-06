caaa476ce0090e611b2e211db6e37fd9825b76c5
bjh3311 <bjh3311@naver.com>
2024-08-06 16:08:27 +0900

    TASK-151 어드민 로그인 서비스 작성 완료

--- src/main/java/newCar/event_page/service/LoginService.java
+++ src/main/java/newCar/event_page/service/LoginService.java
@@ -2,34 +2,58 @@ package newCar.event_page.service;
 
 import lombok.RequiredArgsConstructor;
 import newCar.event_page.exception.AdminLoginFailException;
+import newCar.event_page.model.Session;
+import newCar.event_page.model.SessionStorage;
 import newCar.event_page.model.dto.AdministratorDTO;
 import newCar.event_page.model.entity.Administrator;
 import newCar.event_page.repository.AdministratorRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
     private final AdministratorRepository administratorRepository;
+    private final SessionStorage sessionStorage;
 
-    public String admin(AdministratorDTO administratorDTO){
+    public ResponseEntity<String> admin(AdministratorDTO administratorDTO){
         Administrator administrator = administratorRepository.findById(1L)
                 .orElseThrow(() -> new NoSuchElementException("관리자 정보가 존재하지 않습니다."));
 
-        if(!isLoginSuccess(administrator, administratorDTO)){
+        if(!isAdminLoginSuccess(administrator, administratorDTO)){
             throw new AdminLoginFailException("아이디 혹은 비밀번호가 맞지 않습니다.");
         }
 
-        //TODO 로그인 세션 주는 로직 추가
+        System.out.println("로그인 성공!");
 
-        return "관리자 로그인 성공";
+        //TODO 로그인 세션 주는 로직 추가
+        String sessionId = UUID.randomUUID().toString();
+        Session session = new Session(sessionId, administratorDTO.getAdminId(), "administrator");
+
+        ResponseCookie cookie = ResponseCookie.from("adminSession", sessionId)
+                .httpOnly(true)
+                .path("/admin")
+                .maxAge(60 * 30)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        System.out.println("여기까지 ㄱㅊ");
+        sessionStorage.addSession(session);
+
+        // ResponseEntity 생성 및 반환
+        return new ResponseEntity<>("관리자 로그인 성공", headers, HttpStatus.OK);
     }
 
-    private boolean isLoginSuccess(Administrator admin, AdministratorDTO dto){
+    private boolean isAdminLoginSuccess(Administrator admin, AdministratorDTO dto){
         if(!admin.getAdminId().equals(dto.getAdminId())) return false;
         if(!admin.getPassword().equals(dto.getPassword())) return false;
         return true;
