408ae173c865b1e44e962e79a4ae187e442dfeb5
장준하 <jangjunha@jangjunhaui-MacBookPro.local>
2024-08-07 17:29:18 +0900

    fix logging

--- src/main/java/newCar/event_page/interceptor/LoggingInterceptor.java
+++ src/main/java/newCar/event_page/interceptor/LoggingInterceptor.java
@@ -24,23 +24,6 @@ public class LoggingInterceptor implements HandlerInterceptor {
             System.out.println(headerName + ": " + request.getHeader(headerName));
         }
 
-        // 파라미터 로그
-        System.out.println("Parameters:");
-        Enumeration<String> parameterNames = request.getParameterNames();
-        while (parameterNames.hasMoreElements()) {
-            String paramName = parameterNames.nextElement();
-            System.out.println(paramName + ": " + request.getParameter(paramName));
-        }
-
-        // 바디 로그 (선택 사항, 바디를 여러 번 읽을 수 없으므로 주의)
-        System.out.println("Body:");
-        StringBuilder body = new StringBuilder();
-        BufferedReader reader = request.getReader();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            body.append(line);
-        }
-        System.out.println(body.toString());
         return true;
     }
 }
