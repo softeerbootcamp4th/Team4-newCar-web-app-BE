504181ce3ae8411adba961ba3c5ff51d019937ab
장준하 <jangjunha@jangjunhaui-MacBookPro.local>
2024-08-08 15:43:19 +0900

    batro 허용

--- src/main/java/newCar/event_page/config/WebConfig.java
+++ src/main/java/newCar/event_page/config/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://*.batro.org:*", "https://*.batro.org:*", "http://1.231.159.76:*", "https://1.231.159.76:*", "http://*.batro.org/:*", "https://*.batro.org/:*")
+                .allowedOriginPatterns("http://www.batro.org", "http://*.batro.org:*", "https://*.batro.org:*", "http://1.231.159.76:*", "https://1.231.159.76:*", "http://*.batro.org/:*", "https://*.batro.org/:*")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .maxAge(3600);
