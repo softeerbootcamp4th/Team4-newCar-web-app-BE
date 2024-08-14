0114fcbb45fcac4fbe32fd5edae06c64c4955c82
bjh3311 <bjh3311@naver.com>
2024-08-14 15:10:30 +0900

    TASK-200 WebConfig에서 dummy-token url 인터셉터 제외

--- src/main/java/newCar/event_page/config/WebConfig.java
+++ src/main/java/newCar/event_page/config/WebConfig.java
@@ -34,7 +34,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/main/**")
                 .excludePathPatterns("/main/login")
                 .excludePathPatterns("/main/event-time")
-                .excludePathPatterns("/main/quiz");
+                .excludePathPatterns("/main/quiz")
+                .excludePathPatterns("/main/dummy-token");
     }
 
 
