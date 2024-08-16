f5a0a647a05704abbd1e495e7c183dd0b46a9ed6
bjh3311 <bjh3311@naver.com>
2024-08-16 20:15:25 +0900

    TASK-200 로그인 끝난 후 redirect_uri 변경

--- src/main/java/newCar/event_page/controller/OAuthController.java
+++ src/main/java/newCar/event_page/controller/OAuthController.java
@@ -54,7 +54,7 @@ public class OAuthController {
         String userId = "&userId=" + jwtTokenProvider.getUserId(accessToken);
 
         // URL에 쿼리 파라미터 추가
-        String fullRedirectUrl = this.redirectUrl + "?" + queryParams + userId;
+        String fullRedirectUrl = "?" + queryParams + userId+"&redirect_uri="+this.redirectUrl;
 
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create(fullRedirectUrl));
