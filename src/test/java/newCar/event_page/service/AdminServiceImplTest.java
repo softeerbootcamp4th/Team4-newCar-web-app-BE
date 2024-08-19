7c092cd11cb02975233d8ec483c4338154e32bce
bjh3311 <bjh3311@naver.com>
2024-08-19 18:00:49 +0900

    TASK-262 AdminService TestCode 틀 작성

---
+++ src/test/java/newCar/event_page/service/AdminServiceImplTest.java
@@ -0,0 +1,49 @@
+package newCar.event_page.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AdminServiceImplTest {
+
+    @Test
+    void getCommonEventDetails() {
+    }
+
+    @Test
+    void updateCommonEventDetails() {
+    }
+
+    @Test
+    void getQuizList() {
+    }
+
+    @Test
+    void updateQuiz() {
+    }
+
+    @Test
+    void drawRacingWinners() {
+    }
+
+    @Test
+    void getRacingWinnerList() {
+    }
+
+    @Test
+    void getPersonalityTestList() {
+    }
+
+    @Test
+    void updatePersonalityTest() {
+    }
+
+    @Test
+    void getQuizWinnerList() {
+    }
+
+    @Test
+    void login() {
+    }
+}
