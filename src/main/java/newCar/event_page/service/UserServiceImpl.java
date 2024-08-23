0065242228f091dd0e606e12eceefa20bc866bc5
bjh3311 <bjh3311@naver.com>
2024-08-23 10:59:35 +0900

    isQuizAvailable.get(quizId) -> get(quizId-1) hotfix

--- src/main/java/newCar/event_page/service/UserServiceImpl.java
+++ src/main/java/newCar/event_page/service/UserServiceImpl.java
@@ -105,12 +105,11 @@ public class UserServiceImpl implements UserService {
         Quiz todayQuiz = quizRepository.findByPostDate(LocalDate.now(ZoneId.of("Asia/Seoul")))
                 .orElseThrow(() -> new NoSuchElementException("오늘 날짜에 해당하는 퀴즈 이벤트가 존재하지 않습니다."));
 
-        if(isQuizAvailable.size()<todayQuiz.getId().intValue()){
+        if(LocalDateTime.now(ZoneId.of("Asia/Seoul"))
+                .isAfter(eventCommonRepository.findById(1L).get().getEndTime())){
             throw new IndexOutOfBoundsException("이벤트 기간이 지났습니다");
         }
 
-        System.out.print("todayQuiz.getId().intValue-1 : ");
-        System.out.println(todayQuiz.getId().intValue()-1);
         if(!isQuizAvailable.get(todayQuiz.getId().intValue()-1)){
             throw new FCFSFinishedException("선착순 퀴즈가 마감되었습니다");
         }//오늘 퀴즈가 마감되었다면
@@ -363,7 +362,7 @@ public class UserServiceImpl implements UserService {
 
         int quizId = Integer.parseInt(todayQuiz.getId().toString());
 
-        if(!isQuizAvailable.get(quizId)){
+        if(!isQuizAvailable.get(quizId-1)){
             map.put("status", UserQuizStatus.END);
             return;
         }//이미 마감되어 있다면
